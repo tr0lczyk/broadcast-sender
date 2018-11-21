ce9ef4f07a7d565a89b919bff87633dc5435b629
Mateusz <30321069+tr0lczyk@users.noreply.github.com>
2018-11-21 15:46:10 +0100

    explicit broadcast ended

--- app/src/main/java/com/olczyk/android/broadcastsender/MainActivity.java
+++ app/src/main/java/com/olczyk/android/broadcastsender/MainActivity.java
@@ -1,9 +1,12 @@
 package com.olczyk.android.broadcastsender;
 
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -13,6 +16,8 @@ import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
+
+import java.util.List;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
@@ -30,31 +35,20 @@ public class MainActivity extends AppCompatActivity {
     @Click
     public void buttonMessage(){
         Intent intent = new Intent("android.olczyk.com.broadcastproject.EXAMPLE_ACTION");
-        intent.putExtra("android.olczyk.com.broadcastproject.EXTRA_TEXT",
-                "Broadcast received");
-        sendBroadcast(intent);
-    }
-
-    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String receivedText =
-                    intent.getStringExtra("android.olczyk.com.broadcastproject.EXTRA_TEXT");
-            message.setText(receivedText);
+//        intent.setClass(this, ExampleBroadcastReceiver2.class);
+//        ComponentName cn = new ComponentName("android.olczyk.com.broadcastproject",
+//                "android.olczyk.com.broadcastproject.ExampleBroadCastReceiver");
+//        intent.setComponent(cn);
+//        intent.setClassName("android.olczyk.com.broadcastproject",
+//                "android.olczyk.com.broadcastproject.ExampleBroadCastReceiver");
+//        intent.setPackage("android.olczyk.com.broadcastproject");
+        PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> infos = packageManager.queryBroadcastReceivers(intent,0);
+        for(ResolveInfo resolveInfo : infos){
+            ComponentName cn = new ComponentName(resolveInfo.activityInfo.packageName,
+                    resolveInfo.activityInfo.name);
+            intent.setComponent(cn);
         }
-    };
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        IntentFilter filter =
-                new IntentFilter("android.olczyk.com.broadcastproject.EXAMPLE_ACTION");
-        registerReceiver(broadcastReceiver, filter);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        unregisterReceiver(broadcastReceiver);
+        sendBroadcast(intent);
     }
 }
